0ca8d0d97d85ab34568a6bf8261c6ef785296cf1
VeraBohatyrenko <bogatyrenkovera@gmail.com>
2021-06-18 02:02:56 +0300

    Добавляет разметку сетки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './sass/main.scss';
+import '../src/partials/grid.css';
+import gridItemTbs from './templates/grid-item.hbs';
+
+const refs = {
+  filmGallery: document.querySelector('.gallery'),
+};
+// console.log(refs.filmGallery);
+// function appendGridGalleryMarkUp() {
+//   refs.filmGallery.insertAdjacentHTML('beforeend', gridItemTbs);
+// }
+
+refs.filmGallery.insertAdjacentHTML('beforeend', gridItemTbs());
