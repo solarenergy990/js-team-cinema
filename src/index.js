54755b542fe9cda004e944a8880d22b570072065
Andrei Lysenkovskiy <andreilysen@ukr.net>
2021-06-18 22:26:25 +0300

    add template

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 // index.js is for imports
 // for every single task teammate creates appropriate js file in js folder
 import './sass/main.scss';
+// fetch('https://api.themoviedb.org/3/movie/popular?api_key=5a9486e7363af1432b87b7a7303a7852').then(
+//   response => {
+//     if (response.ok) {
+//       // console.log(page);
+//       // this.page += 1;
+//       return response.json();
+//     }
+//     throw new Error('Error fetching data');
+//   },
+// );
