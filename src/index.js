9930078c13c9418cd4957183a4fbc1f4178cd0c7
VeraBohatyrenko <bogatyrenkovera@gmail.com>
2021-06-19 13:03:15 +0300

    Добавила новую ветку

--- src/index.js
+++ src/index.js
@@ -2,15 +2,4 @@
 // for every single task teammate creates appropriate js file in js folder
 import './sass/main.scss';
 import './js/header';
-import galleryItemTbs from './templates/gallery-item.hbs';
-
-const refs = {
-  filmGallery: document.querySelector('.gallery'),
-};
-// console.log(refs.filmGallery);
-// function appendGridGalleryMarkUp() {
-//   refs.filmGallery.insertAdjacentHTML('beforeend', gridItemTbs);
-// }
-
-refs.filmGallery.insertAdjacentHTML('beforeend', galleryItemTbs());
-
+import './js/gallery';
