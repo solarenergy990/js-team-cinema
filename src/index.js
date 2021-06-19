bace9b84e1315e03f7b3c7a401e4fcf4433129fe
Eugen Soliar <74872776+solarenergy990@users.noreply.github.com>
2021-06-19 10:05:41 +0200

    Revert "Feature grid"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './sass/main.scss';
-import galleryItemTbs from './templates/gallery-item.hbs';
-
-const refs = {
-  filmGallery: document.querySelector('.gallery'),
-};
-// console.log(refs.filmGallery);
-// function appendGridGalleryMarkUp() {
-//   refs.filmGallery.insertAdjacentHTML('beforeend', gridItemTbs);
-// }
-
-refs.filmGallery.insertAdjacentHTML('beforeend', galleryItemTbs());
