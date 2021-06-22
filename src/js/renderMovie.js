2e2ff89679a57ac0a4dca648b165ed4b76604d8a
Andrei Lysenkovskiy <andreilysen@ukr.net>
2021-06-23 00:33:12 +0300

    производит поиск по input

--- src/js/renderMovie.js
+++ src/js/renderMovie.js
@@ -1,16 +1,16 @@
 import MovieSearch from './apiService.js';
 import filmCardTemplate from '../templates/filmCardTemplate.hbs';
-const refs = {
-  filmGallery: document.querySelector('.gallery'),
-};
-const GENRES = [];
-const movieSearch = new MovieSearch();
+import getRefs from './getRefs.js';
 
+const refs = getRefs();
+
+const movieSearch = new MovieSearch();
 
 const renderMovie = async movies => {
   const films = movies.results;
   const genres = await movieSearch.fetchGenresMovie();
   films.map(film => {
+    // console.log(film.release_date.slice(0, 4));
     film.release_date = film.release_date.slice(0, 4);
   });
   films.map(film => {
@@ -21,4 +21,17 @@ const renderMovie = async movies => {
 
   refs.filmGallery.insertAdjacentHTML('beforeend', filmCardTemplate(films));
 };
-const movie = movieSearch.fetchPopularMovie().then(renderMovie);
+
+movieSearch.fetchPopularMovie().then(renderMovie);
+
+const onSearchFilm = async e => {
+  e.preventDefault();
+  movieSearch.query = e.target.elements.query.value;
+  refs.filmGallery.innerHTML = '';
+  movieSearch.resetPage();
+  await movieSearch.fetchMovieSearch().then(renderMovie);
+  // refs.filmGallery.insertAdjacentHTML('beforeend', filmCardTemplate(films));
+  // console.log(movieSearch.query);
+};
+refs.searchFilm.addEventListener('submit', onSearchFilm);
+// console.log(query);
