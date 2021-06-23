043fe4b13172077b3f24d30ef3b4e0ec0838580d
Andrei Lysenkovskiy <andreilysen@ukr.net>
2021-06-23 22:26:04 +0300

    пофиксил отрисовку с первой странички + спинер

--- src/js/renderMovie.js
+++ src/js/renderMovie.js
@@ -1,25 +1,30 @@
 import MovieSearch from './apiService.js';
 import filmCardTemplate from '../templates/filmCardTemplate.hbs';
 import getRefs from './getRefs.js';
-
+import Spinner from './spinner.js';
 
 const refs = getRefs();
+const spinner = new Spinner({
+  selector: '.spinner',
+});
 
 const movieSearch = new MovieSearch();
-
 
 const renderMovie = async movies => {
   const films = movies.results;
   const genres = await movieSearch.fetchGenresMovie();
+  spinner.enable();
   films.map(film => {
-    // console.log(film.release_date.slice(0, 4));
-    film.release_date = film.release_date.slice(0, 4);
+    if (film.release_date !== undefined) {
+      film.release_date = film.release_date.slice(0, 4);
+    }
   });
   films.map(film => {
     film.genre_ids = film.genre_ids.map(
       idSearch => ` ` + genres.find(genre => genre.id === idSearch).name,
     );
   });
+  spinner.disable();
 
   refs.filmGallery.insertAdjacentHTML('beforeend', filmCardTemplate(films));
 };
