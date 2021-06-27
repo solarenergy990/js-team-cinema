f67af9199b3760257919e76cdca22e3749bfc90d
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-06-27 23:15:58 +0200

    exports function

--- src/js/queryMovies.js
+++ src/js/queryMovies.js
@@ -1,16 +1,20 @@
 import getRefs from './getRefs.js';
 // import fetchMovies from './fetchMovies';
 import MovieSearch from './apiService.js';
-import { renderMovie } from './renderMovie';
+import { renderMovie, onSearchFilm } from './renderMovie';
 
 const movieSearch = new MovieSearch();
 const refs = getRefs();
 
 function queryMovies() {
   refs.gallery.innerHTML = '';
+  console.log('this is moviesearch query');
 
-  if (movieSearch.pageQuery.length > 0) {
+  if (movieSearch.query != '') {
     // refs.searchBtn.click();
+
+    console.log('this is moviesearch query', movieSearch.query);
+    movieSearch.onSearchFilm();
   } else {
     movieSearch.fetchPopularMovie().then(renderMovie);
   }
