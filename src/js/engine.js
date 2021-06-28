35c743b7fba4ce46c766518cc4b8d27807648db5
Eugen Soliar <74872776+solarenergy990@users.noreply.github.com>
2021-06-28 20:40:50 +0200

    Revert "працюю над бібліотекою"

--- src/js/engine.js
+++ src/js/engine.js
@@ -13,7 +13,6 @@ const onWatchedLibraryClick = evt => {
 
 addEventListener('click', onWatchedLibraryClick);
 
-
 const onQueueLibraryClick = evt => {
   if (evt.target.classList.contains('js-watched-que')) {
     renderQueueBtn();
@@ -22,10 +21,8 @@ const onQueueLibraryClick = evt => {
 
 addEventListener('click', onQueueLibraryClick);
 
-
 refs.btnMyLibrary.addEventListener('click', () => {
   refs.containerWatchedFilms.innerHTML = ' ';
-  renderWatchedBtn();
 });
 
 function renderWatchedBtn() {
@@ -51,32 +48,16 @@ function renderQueueBtn() {
 }
 
 const renderWatched = doc => {
-  checkID = doc;
-  // console.log(doc.id);
-  // const addDeleteByIdClick = evt => {
-  //   if (evt.target.classList.contains(`js-add-to-delete`)) {
-  //     addDeleteById();
-  //   }
-  // };
-  // addEventListener('click', addDeleteByIdClick);
-  // function addDeleteById() {
-  //   console.log('delete');
-  // }
+  console.log(doc.id);
+  const addDeleteByIdClick = evt => {
+    if (evt.target.classList.contains(`js-add-to-delete`)) {
+      addDeleteById();
+    }
+  };
+  addEventListener('click', addDeleteByIdClick);
+  function addDeleteById() {
+    console.log('delete');
+  }
   const li = filmCard(doc.data());
   refs.containerWatchedFilms.insertAdjacentHTML('beforeend', li);
-
-  // // Click delete user 
-  // // const btnDelete = document.querySelector(`[data-id='${doc.id}'] .btn-delete`)
-  // // btnDelete.addEventListener('click', () => {
-  // //   console.log(doc.id)
-  // })
 };
-
-
-// const onWatchedLibraryClick = evt => {
-//   if (evt.target.classList.contains('js-watched-btn')) {
-//     renderWatchedBtn();
-//   }
-// };
-
-// addEventListener('click', onWatchedLibraryClick);
