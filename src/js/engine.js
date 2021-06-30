e1784fadf8a4dd709b0aef68134fb60eff679ffb
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-06-30 17:45:18 +0200

    db.coll able to discard

--- src/js/engine.js
+++ src/js/engine.js
@@ -1,42 +1,44 @@
-import filmCard from '../templates/filmCardFirebase.hbs';
-import getRefs from './getRefs';
-import { successNotificationWatchedRemove, successNotificationQueueRemove } from './pnotify';
+import filmCard from "../templates/filmCardFirebase.hbs";
+import getRefs from "./getRefs";
+import {
+  successNotificationWatchedRemove,
+  successNotificationQueueRemove,
+} from "./pnotify";
 
 const refs = getRefs();
 
-const onWatchedLibraryClick = evt => {
-  if (evt.target.classList.contains('js-watched-btn')) {
+const onWatchedLibraryClick = (evt) => {
+  if (evt.target.classList.contains("js-watched-btn")) {
     renderWatchedBtn();
     const btnRefs = onBtnDelete();
-    btnRefs.watched.classList.add('button-active');
-    btnRefs.queue.classList.remove('button-active');
+    btnRefs.watched.classList.add("button-active");
+    btnRefs.queue.classList.remove("button-active");
   }
 };
 
-addEventListener('click', onWatchedLibraryClick);
+addEventListener("click", onWatchedLibraryClick);
 
-const onQueueLibraryClick = evt => {
-  if (evt.target.classList.contains('js-watched-que')) {
+const onQueueLibraryClick = (evt) => {
+  if (evt.target.classList.contains("js-watched-que")) {
     renderQueueBtn();
     const btnRefs = onBtnDelete();
-    btnRefs.queue.classList.add('button-active');
-    btnRefs.watched.classList.remove('button-active');
+    btnRefs.queue.classList.add("button-active");
+    btnRefs.watched.classList.remove("button-active");
   }
 };
 
-addEventListener('click', onQueueLibraryClick);
+addEventListener("click", onQueueLibraryClick);
 
-refs.btnMyLibrary.addEventListener('click', () => {
-  refs.containerWatchedFilms.innerHTML = ' ';
+refs.btnMyLibrary.addEventListener("click", () => {
+  refs.containerWatchedFilms.innerHTML = " ";
 });
 
-async function renderWatchedBtn() {
-  refs.containerWatchedFilms.innerHTML = ' ';
-  await db
-    .collection('watched')
+function renderWatchedBtn() {
+  refs.containerWatchedFilms.innerHTML = " ";
+  db.collection("watched")
     .get()
-    .then(querySnapshot => {
-      querySnapshot.forEach(doc => {
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
         renderWatched(doc);
       });
     });
@@ -45,58 +47,58 @@ async function renderWatchedBtn() {
 function onBtnDelete(id, collection) {
   const refs = {
     btnDelete: document.querySelector(`[data-film="${id}"]`),
-    watched: document.querySelector('.js-watched-btn'),
-    queue: document.querySelector('.js-watched-que'),
-  }
+    watched: document.querySelector(".js-watched-btn"),
+    queue: document.querySelector(".js-watched-que"),
+  };
   return refs;
 }
 
 function renderQueueBtn() {
-  refs.containerWatchedFilms.innerHTML = ' ';
-  db.collection('queue')
+  refs.containerWatchedFilms.innerHTML = " ";
+  db.collection("queue")
     .get()
-    .then(querySnapshot => {
-      querySnapshot.forEach(doc => {
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
         renderWatched(doc);
       });
     });
 }
 
-const renderWatched = async doc => {
+const renderWatched = async (doc) => {
   const li = await filmCard(doc.data());
-  await refs.containerWatchedFilms.insertAdjacentHTML('beforeend', li);
+  await refs.containerWatchedFilms.insertAdjacentHTML("beforeend", li);
   const id = await doc.data().id;
   const btnRefs = onBtnDelete(id, doc.collection);
-  const addDeleteWatchedByIdClick = e => {
-    if (e.target.classList.value === 'watched btnremove') {
+  const addDeleteWatchedByIdClick = (e) => {
+    if (e.target.classList.value === "watched btnremove") {
       addDeleteWatchedById();
-    } else if (e.target.classList.value === 'queue btnremove') {
+    } else if (e.target.classList.value === "queue btnremove") {
       addDeleteQueueById();
     }
   };
 
   function addDeleteWatchedById() {
-    db.collection('watched')
+    db.collection("watched")
       .doc(`${doc.id}`)
       .delete()
       .then(() => {
         renderWatchedBtn();
         successNotificationWatchedRemove();
       })
-      .catch(error => console.log('error doc', error));
+      .catch((error) => console.log("error doc", error));
   }
 
   function addDeleteQueueById() {
-    db.collection('queue')
+    db.collection("queue")
       .doc(`${doc.id}`)
       .delete()
       .then(() => {
         renderQueueBtn();
         successNotificationQueueRemove();
       })
-      .catch(error => console.log('error doc', error));
+      .catch((error) => console.log("error doc", error));
   }
-  btnRefs.btnDelete.addEventListener('click', addDeleteWatchedByIdClick);
+  btnRefs.btnDelete.addEventListener("click", addDeleteWatchedByIdClick);
 };
 
-export {renderWatchedBtn, renderQueueBtn}
+export { renderWatchedBtn, renderQueueBtn };
